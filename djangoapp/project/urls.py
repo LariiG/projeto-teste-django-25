c3960db172cd8adf1dbb8b6d51c31766e3d22172
LariiG <laribrun215@hotmail.com>
2025-03-01 16:03:46 -0300

    Pequenas alteracoesna configuracao da urls para a pag Adm

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from .views import index_view
 
 urlpatterns = [
-        path('', index_view.home, name='index'),  # Adicionando a URL para a página inicial
+        path('', index_view, name='index'),  # Adicionando a URL para a página inicial
         path('admin/', admin.site.urls),     # Página de administração
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Para servir arquivos estáticos (se necessário)
 
