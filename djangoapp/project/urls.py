f75c915b008d4786f33fa4e877b20547566c3d16
LariiG <laribrun215@hotmail.com>
2025-02-28 23:05:10 -0300

    Configuracao Url

--- djangoapp/project/urls.py
+++ djangoapp/project/urls.py
@@ -1,29 +1,13 @@
-"""
-URL configuration for project project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .views import index_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Página inicial
-    path('admin_page/', views.admin_page, name='admin_page'),  # Página de administração personalizada
+        path('', index_view.home, name='index'),  # Adicionando a URL para a página inicial
+        path('admin/', admin.site.urls),     # Página de administração
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # Para servir arquivos estáticos (se necessário)
 
 #if settings.DEBUG:
--- djangoapp/project/views.py
+++ djangoapp/project/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
-# Página inicial
-def home(request):
-    return render(request, 'home.html')
-
-@login_required
-def admin_page(request):
-    return render(request, 'admin_page.html')
+def index_view(request):
+    return render(request, 'index.html')
